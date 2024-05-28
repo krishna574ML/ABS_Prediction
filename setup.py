351b6a878ea5cb76123b8c856bc9635b8d59d572
krishna574ML <saik36048@gmail.com>
2024-05-28 14:22:38 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup , find_packages
+
+setup(
+    name = 'ABS_ml_project'
+    version='1.0.0'
+    author='Krishna',
+    author_email='saik36048@gmail.com',
+    packages=find_packages() ,
+    #install_requires =get_requirments('requriments.txt')
+
+    
+    )
