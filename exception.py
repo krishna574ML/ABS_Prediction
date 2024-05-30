962656ef7f03c078957f0f0b11db560be346aaeb
krishna574ML <saik36048@gmail.com>
2024-05-30 18:10:16 +0530

    2nd commit

---
+++ exception.py
@@ -0,0 +1,23 @@
+import sys
+
+
+def error_message_details(error_message , error_details:sys):
+    _,_, exc_tb = error_details.exc_info()
+    filename = exc_tb.tb_frame.f_code.co_filename
+    linenumber = exc_tb.tb_lineno
+    error = str(error_message)
+    error_message = f"error occured in the file{filename} on the line{linenumber} with error{error}"
+    return error_message
+
+    
+    
+    
+class SensorException(Exception):
+    
+    def __init__(self , error_message , error_details:sys):
+        super().__init__(error_message)
+        self.error_message = error_message_details(error_message , error_details=error_details)
+        
+        
+    def __str__(self):
+        return self.error_message
--- setup.py
+++ setup.py
@@ -1,12 +1,23 @@
 from setuptools import setup , find_packages
 
+
+def get_requirements(file_path:str) ->list[str]:
+    
+    requriments=[]
+    hypen_e_dot = '-e .'
+    with open(file_path) as file_obj:
+        requriments = file_obj.readlines()
+        requriments = [i.replace('\n' , "" )for i in requriments] # type: ignore
+
+        if hypen_e_dot in requriments:
+            requriments.remove(hypen_e_dot)
+    return requriments
+
 setup(
-    name = 'ABS_ml_project'
-    version='1.0.0'
+    name='ABS_ml_project',
+    version='1.0.0',
     author='Krishna',
     author_email='saik36048@gmail.com',
-    packages=find_packages() ,
-    #install_requires =get_requirments('requriments.txt')
-
-    
-    )
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
+    ) 
